376527844153a46435e5ff5324cb373f183ab9c4
Luke Rushworth <ldjrushworth@icloud.com>
2019-08-01 19:33:29 +0100

    removes graphql-migrate dependency

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql");
-// const { GraphQLSchema } = require("graphql");
-// const { migrate } = require("graphql-migrate");
-// const dbConfig = require("./knexfile");
-// const { query } = require("./schema/index-query");
-// const { mutation } = require("./schema/index-mutations");
 const { schema, root } = require("./schema");
-// const { types } = require('./schema/index-type')
-
-// const schema = new GraphQLSchema({
-//   query,
-//   mutation
-//   // types
-// });
-
-// migrate(dbConfig, schema).then(() => {
-//   console.log("your database is up to date");
-// });
 
 const app = express();
 
