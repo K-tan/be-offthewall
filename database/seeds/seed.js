d6efaa99f0c0757f89e8ddcb624403b3f582fc2a
Luke Rushworth <ldjrushworth@icloud.com>
2019-08-06 09:27:31 +0100

    adds hashing to artist password data

--- database/seeds/seed.js
+++ database/seeds/seed.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
 const database = require("../../connection");
 const {
   artistsData,
@@ -12,7 +14,18 @@ exports.seed = () => {
     .rollback()
     .then(() => database.migrate.latest())
     .then(() => {
-      const artistsInsertions = database("artists").insert(artistsData);
+      let hashedData = [...artistsData];
+      let salt = bcrypt.genSaltSync(saltRounds);
+      hashedData.forEach((artist, index) => {
+        if (artist.password === undefined) {
+          hashedData[index].artist_password = "password";
+        }
+        hashedData[index].artist_password = bcrypt.hashSync(
+          artist.artist_password,
+          salt
+        );
+      });
+      const artistsInsertions = database("artists").insert(hashedData);
       const wallsInsertions = database("walls").insert(wallsData);
       const consumersInsertions = database("consumers").insert(consumersData);
       return Promise.all([
