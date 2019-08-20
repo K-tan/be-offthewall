0ff4fdcb718689c2c1a0ee4b86166fa4d17748c6
Simon Davey <simonr.davey@gmail.com>
2019-08-20 16:40:03 +0100

    Restores use of dotenv package.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 const { ApolloServer, gql } = require("apollo-server");
 const database = require("./connection");
 const { getUser } = require("./utils");
